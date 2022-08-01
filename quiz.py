4e4a7d03ed99e01a95e25f978a86cdcc8f3d459d
James Tucker <j.tucker07@gmail.com>
2022-08-01 20:54:21 +0100

    add % correct

--- quiz.py
+++ quiz.py
@@ -3,18 +3,20 @@ import random
 
 lives = 5
 score = 0
+qcount = 0
 
 while lives > 0:
     qchoice = random.choices(questions)
     for q,a in qchoice:
         answer = input(f"{q}""?").lower()
+        qcount +=1
         if answer == a.lower():
             print("Correct! Well done")
-            score = score + 1
+            score +=1
         else:
-            lives = lives - 1
+            lives -=1
             print(f"Sorry! That's incorrect. You have {lives} lives left.")
         
 
 
-print(f"Oops, you died. You got {score} questions correct.")
+print(f"Oops, you died. You got {score} ({score/qcount*100}%) questions correct.")
